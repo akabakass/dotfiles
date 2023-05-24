94fd3f880dc873f61a94f75f0544f6c5e3ce50ff
jc <jc@tubs.fr>
2023-05-24 07:48:25 +0200

    put lsp and mason configuration inside pcall check to not stop all script on a missing extension

--- nvim/after/plugin/lsp_management.lua
+++ nvim/after/plugin/lsp_management.lua
@@ -1,18 +1,18 @@
 -- order of plugin is important here
 local mason_status, mason = pcall(require, "mason")
-if not mason_status then
+if mason_status then
+  mason.setup({
+    ui = {
+      border = "rounded"
+    }
+  })
   return
 end
 
-mason.setup({
-  ui = {
-    border = "rounded"
-  }
-})
 
 local mason_lsp_status, mason_lsp = pcall(require, "mason-lspconfig")
-if not mason_lsp_status then
-  return
+if mason_lsp_status then
+  mason_lsp.setup()
 end
 
 local mason_tools_status, mason_tools = pcall(require, "mason-tool-installer")
@@ -23,21 +23,26 @@ if mason_tools_status then
   })
 end
 
-local servers = {
-  "lua_ls",
-  "cssls",
-  "html",
-  "tsserver",
-  "pyright",
-  "bashls",
-  "jsonls",
-  "yamlls",
-  "intelephense"
-}
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-for _, server in ipairs(servers) do
-  lsp[server].setup {
-    capabilities = capabilities
+local lsp_status, lsp = pcall(require, "lspconfig")
+if lsp_status then
+  local servers = {
+    "lua_ls",
+    "cssls",
+    "html",
+    "tsserver",
+    "sqlls",
+    "pyright",
+    "bashls",
+    "jsonls",
+    "yamlls",
+    "intelephense"
   }
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+  for _, server in ipairs(servers) do
+    lsp[server].setup {
+      capabilities = capabilities
+    }
+  end
 end
+
