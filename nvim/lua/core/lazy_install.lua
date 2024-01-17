81a2fae3712dc5cb156d75378c7b4dea572db482
jc <jc@tubs.fr>
2024-01-17 13:20:45 +0100

    allow to solve leetcode inside nvim

--- nvim/lua/core/lazy_install.lua
+++ nvim/lua/core/lazy_install.lua
@@ -98,6 +98,23 @@ return lazy.setup({
     }
   },
   {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim", -- required by telescope
+        "MunifTanjim/nui.nvim",
+
+        -- optional
+        "rcarriga/nvim-notify",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+        -- configuration goes here
+    }
+  },
+  {
     '2kabhishek/nerdy.nvim',
     dependencies = {
         'stevearc/dressing.nvim',
