e0df165a4ffd9087ee85810e75997729c26cd231
jc <jc@tubs.fr>
2024-02-22 14:55:40 +0100

    add autopairs plugin

---
+++ nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,18 @@
+return {
+  'altermo/ultimate-autopair.nvim',
+  event={
+    'InsertEnter',
+    'CmdlineEnter'
+  },
+  opts = {
+    space2 = {
+      enable = true
+    },
+    tabout = {
+      enable = true
+    },
+    close = {
+      enable = true
+    },
+  }
+}
