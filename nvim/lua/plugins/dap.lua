cf1387398a61a97fc731a2a00960cadd90fa209a
jc <jc@tubs.fr>
2024-03-25 14:04:36 +0100

    add nvim dap support

---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,55 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio"
+  },
+  config = function()
+    local dap = require('dap')
+    local dap_ui = require('dapui')
+    dap_ui.setup()
+    dap.listeners.before.attach.dapui_config = function()
+      dap_ui.open()
+    end
+    dap.listeners.after.event_initialized.dapui_config = function()
+      dap_ui.open({
+        reset = true
+      })
+    end
+    dap.listeners.before.event_terminated.nvim_dap_ui_config = function()
+      dap_ui.close()
+    end
+    dap.listeners.before.event_exited.nvim_dap_ui_config = function()
+      dap_ui.close()
+    end
+
+
+    dap.adapters.php = {
+      type = 'executable',
+      command = 'node',
+      args = {
+        '/home/jc/dotfiles/nvim/src/vscode-php-debug/out/phpDebug.js'
+      }
+    }
+
+    dap.configurations.php = {
+      {
+        type = 'php',
+        request = 'launch',
+        name = 'Listen for Xdebug',
+        port = 9001
+      }
+    }
+
+    dap.configurations.javascript = {
+      {
+        type = "pwa-node",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        cwd = "${workspaceFolder}"
+      }
+    }
+
+  end
+}
