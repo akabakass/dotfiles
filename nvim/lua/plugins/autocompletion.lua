c98f0fa2d03f8960097e86da4f8763b990096bb0
jc <jc@tubs.fr>
2024-05-21 11:40:56 +0200

    add lspkind icons to cmp

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -2,6 +2,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
+      "onsails/lspkind.nvim",
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
       "rafamadriz/friendly-snippets",
