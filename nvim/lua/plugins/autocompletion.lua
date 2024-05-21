16185077d006029b1d5f0131cf1864015824b2a7
jc <jc@tubs.fr>
2024-05-21 11:32:38 +0200

    fix and improve nvim-cmp

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -17,6 +17,7 @@ return {
       }
     },
     config = function()
+      vim.opt.shortmess:append("c")
       local cmp = require('cmp')
       local luasnip = require('luasnip')
       require("luasnip.loaders.from_vscode").lazy_load() -- to use friendly-snippets
@@ -48,19 +49,14 @@ return {
         TypeParameter = "",
       }
 
-      local check_backspace = function()
-        local col = vim.fn.col "." - 1
-        return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-      end
-
       cmp.setup({
         completion = {
-        -- autocomplete = {
-        -- cmp.TriggerEvent.TextChanged,
-        -- cmp.TriggerEvent.InsertEnter
-        -- },
-        -- completeopt = "menuone,noinsert,noselect",
-        -- keyword_length = 0
+          autocomplete = {
+            cmp.TriggerEvent.TextChanged,
+            cmp.TriggerEvent.InsertEnter
+          },
+          completeopt = "menuone,noinsert,noselect",
+          -- keyword_length = 0
           max_item_count = 3
         },
         snippet = {
@@ -73,85 +69,74 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = {
-          ["<C-k>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-          ["<C-j>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-          ["<C-_>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-          ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+          ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+          ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+          ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+          ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+          ["<CR>"] = cmp.mapping(cmp.mapping.confirm({
+            cmp.ConfirmBehavior.insert,
+            select = true
+          }), { "i", "c" }),
           ["<C-e>"] = cmp.mapping {
             i = cmp.mapping.abort(),
             c = cmp.mapping.close()
-          },
-          ["<CR>"] = cmp.mapping(cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true
-          })),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-            cmp.select_next_item()
-            -- elseif luasnip.expandable() then
-              -- luasnip.expand()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif check_backspace() then
-              fallback()
-            else
-              fallback()
-            end
-          end, {
-          "i",
-          "s",
+          }
+        },
+        sources = cmp.config.sources({
+          { name = "cmp-dbee" },
+          { name = 'nvim_lsp_signature_help' },
+          { name = 'nvim_lua' },
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'path' },
+          { name = 'buffer', keyword_length = 5 }
+        },
+        {
+          { name = 'buffer'}
         }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
+        formatting = {
+          fields = {  "abbr", "menu", "kind" },
+          format = function(entry, vim_item)
+            -- Kind icons
+            -- vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+            vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+            vim_item.menu = ({
+              nvim_lsp = "[LSP]",
+              rg = "[RG]",
+              luasnip = "[Snippet]",
+              luasnip_choice = "[Snippet]",
+              buffer = "[Buffer]",
+              path = "[Path]",
+              cmdline = "[Cmdline]",
+              cmdline_history = "[Cmdline_History]"
+            })[entry.source.name]
+            return vim_item
           end
-        end, {
-        "i",
-        "s",
+        }
       })
-    },
-    sources = cmp.config.sources({
-      { name = "cmp-dbee" },
-      { name = 'nvim_lsp_signature_help' },
-      { name = 'nvim_lua' },
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' },
-      { name = 'path' },
-      { name = 'buffer', keyword_length = 5 }
-    },
-    {
-      { name = 'buffer'}
-    }),
-    formatting = {
-      fields = {  "abbr", "menu", "kind" },
-      format = function(entry, vim_item)
-        -- Kind icons
-        -- vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-        vim_item.menu = ({
-          nvim_lsp = "[LSP]",
-          rg = "[RG]",
-          luasnip = "[Snippet]",
-          luasnip_choice = "[Snippet]",
-          buffer = "[Buffer]",
-          path = "[Path]",
-          cmdline = "[Cmdline]",
-          cmdline_history = "[Cmdline_History]"
-        })[entry.source.name]
-        return vim_item
-      end
-    },
-    -- confirm_opts = {
-      -- behavior = cmp.ConfirmBehavior.Replace,
-      -- select = false
-    -- },
-  })
-
-end
-
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'cmdline' },
+          { name = 'path' },
+        }
+      })
+      Key({'i', 's'}, "<Tab>", function()
+        if  luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        end
+      end, {silent = true})
+      Key({'i', 's'}, "<S-Tab>", function()
+        if  luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        end
+      end, {silent = true})
+    end
   }
 }
