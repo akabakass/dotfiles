2aae1b9e2b7cd7bde7a85cfeea1f8f9d5f28d026
jc <jc@tubs.fr>
2024-06-04 16:13:36 +0200

    add conform formatting plugin

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,22 @@
+return {
+  'stevearc/conform.nvim',
+  config = function()
+    local conform = require("conform")
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { "prettier" },
+        html = { "prettier" },
+        php = { "easy-coding-standard" }
+      }
+    })
+    Key({"n", "v"}, "<leader>mp", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500
+      })
+    end
+    , Opts("[M]ake [P]retty"))
+  end
+}
