bfc8092c6e60c752a999fdd0ae7f05998d518138
jc <jc@tubs.fr>
2025-06-03 11:04:19 +0200

    change autopair plugins

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,18 +1,26 @@
 return {
-  'altermo/ultimate-autopair.nvim',
-  event={
-    'InsertEnter',
-    'CmdlineEnter'
+  {
+    'm4xshen/autoclose.nvim',
+    config = true
   },
-  opts = {
-    space2 = {
-      enable = true
-    },
-    tabout = {
-      enable = true
-    },
-    close = {
-      enable = true
-    },
+  {
+    "windwp/nvim-ts-autotag",
+    config = true
   }
+  --  'altermo/ultimate-autopair.nvim',
+  --  event={
+  --    'InsertEnter',
+  --    'CmdlineEnter'
+  --  },
+  --  opts = {
+  --    space2 = {
+  --      enable = true
+  --    },
+  --    tabout = {
+  --      enable = true
+  --    },
+  --    close = {
+  --      enable = true
+  --    },
+  --  }
 }
