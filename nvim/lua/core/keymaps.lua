a0b935bb0293f01b50abc6c1ac00b4cf050847b7
jc <jc@tubs.fr>
2025-06-03 11:02:13 +0200

    improve find files for telescope

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -43,8 +43,33 @@ Key("n", "<C-a>", ":NvimTreeFindFileToggle<CR>", Opts())
 
 local telescope_b_status, telescope_b = pcall(require, "telescope.builtin")
 if telescope_b_status then
+  local my_find_files
+  my_find_files = function(opts, no_ignore)
+    opts = opts or {}
+    no_ignore = vim.F.if_nil(no_ignore, false)
+    opts.attach_mappings = function(_, map)
+      map({ "n", "i" }, "<C-f>", function(prompt_bufnr) -- <C-h> to toggle modes
+        local prompt = require("telescope.actions.state").get_current_line()
+        require("telescope.actions").close(prompt_bufnr)
+        no_ignore = not no_ignore
+        my_find_files({ default_text = prompt }, no_ignore)
+      end)
+      return true
+    end
+
+    if no_ignore then
+      opts.no_ignore = true
+      opts.hidden = true
+      opts.prompt_title = "Find Files <ALL>"
+      require("telescope.builtin").find_files(opts)
+    else
+      opts.prompt_title = "Find Files"
+      require("telescope.builtin").find_files(opts)
+    end
+  end
+
   Key("n", "<leader>tt", telescope_b.resume, Opts("resume [TT]elescope"))
-  Key("n", "<leader>ff", telescope_b.find_files, Opts("[F]ind [F]iles"))
+  Key("n", "<leader>ff", my_find_files, Opts("[F]ind [F]iles"))
   Key("n", "<leader>gl", telescope_b.live_grep, Opts("[G]rep [L]ive"))
   Key("n", "<leader>gs", telescope_b.grep_string, Opts("[G]rep [S]tring"))
   Key("n", "<leader>fh", telescope_b.help_tags, Opts("[F]ind [H]elp"))
