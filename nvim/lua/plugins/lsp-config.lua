b3976653dd2d4d47d90fd820874192ea14713603
jc <jc@tubs.fr>
2025-06-03 12:11:42 +0200

    WIP: cleanup mason

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -23,46 +23,37 @@ return {
     config = function()
       local mason_lsp = require("mason-lspconfig")
       mason_lsp.setup({
-        auto_install = true
+        auto_install = true,
+        automatic_enable = true
       })
-      mason_lsp.setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function (server_name) -- default handler (optional)
-          -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
-          local capabilities = require('blink.cmp').get_lsp_capabilities()
-          local lsp_config = require("lspconfig")
-          lsp_config[server_name].setup ({
-            root_dir = function(file_name)
-              return lsp_config.util.find_git_ancestor(file_name)
-            end,
---            capabilities = capabilities
-          })
-        end,
-        ['html'] = function()
-          local lspconfig = require('lspconfig')
-          lspconfig.html.setup({
-            filetypes = {
-              "php",
-              "html"
-            }
-          })
-        end
+      vim.lsp.config.intelephense = {
+
       }
+      --mason_lsp.setup_handlers {
+      --  -- The first entry (without a key) will be the default handler
+      --  -- and will be called for each installed server that doesn't have
+      --  -- a dedicated handler.
+      --  function (server_name) -- default handler (optional)
+      --    -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      --    local capabilities = require('blink.cmp').get_lsp_capabilities()
+      --    local lsp_config = require("lspconfig")
+      --    lsp_config[server_name].setup ({
+      --      root_dir = function(file_name)
+      --        return lsp_config.util.find_git_ancestor(file_name)
+      --      end,
+--    --        capabilities = capabilities
+      --    })
+      --  end,
+      --  ['html'] = function()
+      --    local lspconfig = require('lspconfig')
+      --    lspconfig.html.setup({
+      --      filetypes = {
+      --        "php",
+      --        "html"
+      --      }
+      --    })
+      --  end
+      --}
     end
-  },
-  {
-    "folke/neodev.nvim",
-    opts = {
-      pathSctrict = false
-    }
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      auto_update = true,
-      run_on_start = true
-    }
   }
 }
