9a61bde27ecf6aabbacbe5d7f2ac1c408edf0af3
jc <jc@tubs.fr>
2025-08-27 10:20:18 +0200

    improve reset_screen script for more compatibility

--- i3-laptop/reset_screens.sh
+++ i3-laptop/reset_screens.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-xrandr --output eDP-1 --off 
+xrandr --output eDP-1 --off
 xrandr --output eDP-1 --auto
 
-xrandr --output HDMI-2 --off 
-xrandr --output HDMI-2 --auto 
+xrandr --output HDMI-2 --off
+xrandr --output HDMI-2 --auto
+xrandr --output DP-2 --off
+xrandr --output DP-2 --auto
 
-xrandr --output eDP-1 --pos 640x1080 
+xrandr --output eDP-1 --pos 640x1080
